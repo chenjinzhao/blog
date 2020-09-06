98c05b01a9cd70ad3ee9ee969fb0f2d1b0ce88b7
chenjinzhao <990736382@qq.com>
2020-09-06 21:45:34 +0800

    first commit

---
+++ src/test/java/cn/chenjinzhao/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chenjinzhao.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
